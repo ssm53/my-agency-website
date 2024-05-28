4f090afa26c31e90ca6116087c378ac4e809a937
Shaun Menezes <shaunshanil95@gmail.com>
2024-05-28 16:46:30 +0800

    enabled whatsapp for navbar button

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -74,7 +74,7 @@ const Navbar = () => {
 
         <div className="hidden md:flex">
           <Link
-            href="https://wa.me/60123397028"
+            href={"https://wa.me/60123397028"}
             className="
             inline-flex h-12 animate-shimmer items-center justify-center 
             rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
